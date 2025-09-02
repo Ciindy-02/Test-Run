529d84f93eb2cd41e0cc150eb89f278905ab8171
Ciindy-02 <cindyhdz1227@gmail.com>
2025-09-02 15:43:16 -0700

    Initial commit: simple Java program

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        
+        // Add a little extra just for testing
+        int a = 5, b = 7;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println("Goodbye, GitHub!");
+    }
+}
+
+
